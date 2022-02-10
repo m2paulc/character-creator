3ff40ad152835b1c0dbdab66f3428ac6fae3275d
m2paulc <hyp3rsp8c3@gmail.com>
2022-02-10 10:59:32 -0500

    fix backdraft

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,8 +30,8 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.backdraft}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
-        <div className={styles.backdraft}></div>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
